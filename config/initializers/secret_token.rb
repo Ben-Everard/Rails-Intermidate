969db3e8d17cd24f6d9076ac0b159bda59c55743
Ben Everard <bfeverard@gmail.com>
2013-06-19 08:57:16 -0700

    Intermidate with out using simple-forms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intermediate::Application.config.secret_token = '1dc0f7f4abbfda4c698a965ca033b16476adb0fc3490a965cf1e14fd97511caa1c68b66afaed9ed1bc2db1364dc7c7de83df23f530a8d45f40ed62e2308e6f6c'
